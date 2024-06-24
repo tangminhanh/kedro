0baa37e8300a99ef5dd8cde57ae5873b5e552185
Lynn <57570218+tangminhanh@users.noreply.github.com>
2024-06-24 20:41:07 +0800

    add data_processing pipeline

--- mlops/src/mlops/pipelines/data_processing/pipeline.py
+++ mlops/src/mlops/pipelines/data_processing/pipeline.py
@@ -1,28 +1,72 @@
 from kedro.pipeline import Pipeline, node, pipeline
+# from kedro.io import MemoryDataset
+# from kedro.runner import SequentialRunner
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
+from .nodes import (
+    dataframe_to_dataset,
+    department_label_encoding,
+    preprocess_function,
+    train_model,
+    split_department_data,
+    split_df_to_dataset
+    # split_subcategory_data
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs=["preprocessed_companies", "companies_columns"],
-                name="preprocess_companies_node",
+                func=split_department_data,
+                inputs=["department_encoded_df", "params:model_options"],
+                outputs=["train_department_df", "test_department_df"],
+                name="split_department_node"
             ),
             node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
+                func=dataframe_to_dataset,
+                inputs=["train_department_df", "test_department_df"],
+                outputs=["train_department_dataset",
+                         "test_department_dataset"],
+                name="dataframe_to_dataset_node"
             ),
             node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
+                func=department_label_encoding,
+                inputs="train_department_dataset",
+                outputs=["department2id", "id2department"],
+                name="department_label_encoding_node"
+            ),
+            node(
+                func=preprocess_function,
+                inputs=["train_department_dataset",
+                        "test_department_dataset", "department2id"],
+                outputs=["tokenized_train_department_dataset",
+                         "tokenized_test_department_dataset"],
+                name="department_tokenization_node"
+            ),
+            node(
+                func=train_model,
+                inputs=["tokenized_train_department_dataset",
+                        "tokenized_test_department_dataset", "department2id", "id2department"],
+                outputs="trained_model",
+                name="train_model_node"
+            ),
+            node(
+                func=split_df_to_dataset,
+                inputs=["techgroup_encoded_dir", "params:model_options"],
+                outputs=["train_techgroup_dir", "test_techgroup_dir"],
+                name="split_techgroup_and_transform_to_dataset_node"
+            ),
+            node(
+                func=split_df_to_dataset,
+                inputs=["category_encoded_dir", "params:model_options"],
+                outputs=["train_category_dir", "test_category_dir"],
+                name="split_category_and_transform_to_dataset_node"
+            ),
+            node(
+                func=split_df_to_dataset,
+                inputs=["subcategory_encoded_dir", "params:model_options"],
+                outputs=["train_subcategory_dir", "test_subcategory_dir"],
+                name="split_subcategory_and_transform_to_dataset_node"
             ),
         ]
     )
